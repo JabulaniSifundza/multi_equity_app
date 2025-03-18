6b495c17676bb4ca57eefdee9743234d2069620c
JabulaniSifundza <jjsifundza@gmail.com>
2025-03-18 15:20:35 -0500

    GenAI API

--- equity_app/equity_super_app.py
+++ equity_app/equity_super_app.py
@@ -12,6 +12,14 @@ import io
 import math
 import streamlit as st
 import sys
+from google import genai
+import datetime
+import numpy as np
+import pandas as pd
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.agents import AgentType, initialize_agent
+from langchain_community.tools.yahoo_finance_news import YahooFinanceNewsTool
+from langchain_core.prompts import PromptTemplate
 
 API_TOKEN = "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"
 API_URL = "https://api-inference.huggingface.co/models/human-centered-summarization/financial-summarization-pegasus"
@@ -178,6 +186,43 @@ else:
                     new_urls.append(res)
             return list(set(new_urls))
         cleaned_urls = {ticker:remove_unwanted_strings(article_links[ticker], unwanted_string_list) for ticker in research_tickers}
+        tools = [YahooFinanceNewsTool()]
+        agent_chain = initialize_agent(
+            tools,
+            llm,
+            agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+            verbose=True,
+        )
+        # agent_chain.invoke(
+        #    "What happened today with Microsoft stocks?",
+        # )
+        tool = YahooFinanceNewsTool()
+        # res = tool.invoke("AAPL")
+        # print(res)
+
+        TICKERS = ["AAPL", "TSLA", "MSFT", "AMZN"]
+        news_articles = []
+
+        for ticker in TICKERS:
+            # agent_chain.invoke(f"What happened today with {ticker} stocks?")
+            news = tool.invoke(ticker)
+            news_articles.append(news)
+            print(news_articles)
+            
+        for news_story in news_articles:
+            template = """Question: {question}
+            
+            Answer: Let's think step by step from the lens of a highly analytical financial analyst 
+            looking for companies to invest in. 
+            Investments can be long or short positions. Analyze the sentiment of the text 
+            and how it might affect stock prices in the near future."""
+            prompt = PromptTemplate.from_template(template)
+            
+            chain = prompt | llm
+            
+            question = f"What's your outlook on {news_story}?"
+            answer = chain.invoke({"question": question})
+            print(answer)
       
         # print(cleaned_urls)
         company_ticker = Ticker(research_ticker)
@@ -334,3 +379,5 @@ else:
     except Exception as Err:
         st.write("An error has occured. Please give the model 20 seconds to load and try again")
         print(Err)
+        
+        
